4be42f69e7034ebf94d5f47943e6ccea8bb4ee95
ming-taro <hera002008@naver.com>
2023-12-02 18:19:35 +0900

    refactor: 재시도 여부를 결정하는 1, 2 매직넘버 처리

--- src/main/java/baseball/constants/ErrorMessage.java
+++ src/main/java/baseball/constants/ErrorMessage.java
@@ -3,9 +3,9 @@ package baseball.constants;
 public enum ErrorMessage {
 
     INVALID_NUMBER("숫자를 다시 입력해 주세요"),
-    INVALID_NUMBER_SIZE(Rule.NUMBER_COUNT.getValue() + "개의 숫자만 입력 가능합니다."),
+    INVALID_NUMBER_SIZE(Rule.NUMBER_COUNT.getValue() + "개의 숫자만 입력 가능 합니다."),
     DUPLICATE_NUMBER("중복 되지 않은 숫자를 입력해 주세요"),
-    INVALID_ATTEMPT("잘못된 입력입니다. 1 또는 2를 입력해 주세요"),
+    INVALID_ATTEMPT("잘못된 입력 입니다."),
     BLANK_INPUT("빈 값은 입력할 수 없습니다.");
 
     private static final String PREFIX = "[ERROR]";
--- src/main/java/baseball/constants/OutputMessage.java
+++ src/main/java/baseball/constants/OutputMessage.java
@@ -3,8 +3,9 @@ package baseball.constants;
 public enum OutputMessage {
     START("숫자 야구 게임을 시작합니다."),
     NUMBER_INPUT("숫자를 입력해주세요 : "),
-    END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    RETRY("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    END(Rule.NUMBER_COUNT.getValue() + "개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    RETRY("게임을 새로 시작하려면" + Rule.RETRY_GAME.getValue()
+            + ", 종료하려면 " + Rule.END_GAME.getValue() + "를 입력하세요."),
     BALL("%d볼"),
     STRIKE("%d스트라이크"),
     NOT_MATCH("낫싱");
