c91b0b077f5623697a79753cda0ae8eb40360675
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-02 17:21:17 +0900

    test : 재시작 여부를 확인할 때 1,2가 입력되지 않으면 예외처리를 하는지 확인하는 테스트 코드 작성함

---
+++ src/test/java/baseball/service/BaseBallServiceTest.java
@@ -0,0 +1,31 @@
+package baseball.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BaseBallServiceTest {
+    private BaseBallService baseBallService;
+
+    @BeforeEach
+    void setUp() {
+        baseBallService = new BaseBallService();
+    }
+
+    @Test
+    void compareNumbers() {
+    }
+
+    @DisplayName("1,2가 입력되지 않으면 예외처리를 하는지")
+    @ParameterizedTest
+    @ValueSource(strings = {"aa", "@sa", "3", "", " "})
+    void isValid(String input) {
+        // given & when & then
+        assertThatThrownBy(() -> baseBallService.isValid(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
