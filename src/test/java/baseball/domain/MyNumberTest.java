10b43470130f6f41c5df76b5b719eca9df653421
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-02 16:23:01 +0900

    test : 리스트 인덱스 값을 가져오는지 확인하는 테스트 코드 구현

--- src/test/java/baseball/domain/MyNumberTest.java
+++ src/test/java/baseball/domain/MyNumberTest.java
@@ -1,13 +1,25 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MyNumberTest {
+
+    private static Stream<Arguments> myNumbers() {
+        return Stream.of(
+                Arguments.arguments(new MyNumber("123"), "123"),
+                Arguments.arguments(new MyNumber("456"), "456"),
+                Arguments.arguments(new MyNumber("789"), "789")
+        );
+    }
 
     @DisplayName("공백을 입력했을 경우 예외처리를 하는지")
     @ParameterizedTest
@@ -27,7 +39,19 @@ class MyNumberTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void getNumberIndex() {
+    @DisplayName("리스트 인덱스 값을 가져오는지")
+    @ParameterizedTest
+    @MethodSource("myNumbers")
+    void getNumberIndex(MyNumber myNumber, String expectedNumbers) {
+        for (int i = 0; i < expectedNumbers.length(); i ++) {
+            // given
+            Integer expected = Character.getNumericValue(expectedNumbers.charAt(i));
+
+            // when
+            Integer actual = myNumber.getNumberIndex(i);
+
+            // then
+            assertThat(expected).isEqualTo(actual);
+        }
     }
 }
