8cd3b8355c158aa8723783146ed6e69af825f2a7
ming-taro <hera002008@naver.com>
2023-12-03 15:13:32 +0900

    test: 이름이 5자를 넘어가면 예외가 발생하는지 테스트

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -3,14 +3,16 @@ package racingcar.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
 
-    @Test
     @DisplayName("두 자동차의 이름이 같다면 같은 자동차 임을 확인 한다")
+    @Test
     void validateDuplicateCar() {
         Car firstCar = new Car("ming");
         Car secondCar = new Car("ming");
@@ -18,12 +20,20 @@ class CarTest {
         Assertions.assertTrue(firstCar.isDuplicateCar(secondCar));
     }
 
-    @Test
     @DisplayName("두 자동차의 이름이 다르다면 다른 자동차 임을 확인 한다")
+    @Test
     void validateNotDuplicateCar() {
         Car firstCar = new Car("ming");
         Car secondCar = new Car("taro");
 
         Assertions.assertFalse(firstCar.isDuplicateCar(secondCar));
+    }
+
+    @DisplayName("이름이 5자를 넘어가면 자동차 객체 생성시 예외가 발생 한다")
+    @ValueSource(strings = {"abcdef", "mingming", "123mingss"})
+    @ParameterizedTest
+    void createCar(String carName) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
