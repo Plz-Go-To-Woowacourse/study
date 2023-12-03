f557c8b753c9c66c403b90382304d5b212a1a388
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-03 14:10:59 +0900

    feat : 움직인 횟수를 바탕으로 결과 객체 생성함

---
+++ src/main/java/racingcar/domain/Result.java
@@ -0,0 +1,20 @@
+package racingcar.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    private final Map<String, Integer> results;
+
+    public Result(RacingCar racingCar, List<Integer> moveResult) {
+        this.results = racingCar.getNameToMoveCountMap(moveResult);
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "results=" + results +
+                '}';
+    }
+}
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,9 +1,14 @@
 package racingcar.view;
 
+import static racingcar.constants.ViewConstants.MOVEMENT_MESSAGE;
 import static racingcar.constants.ViewConstants.START_MESSAGE;
 
 public class OutputView {
     public void printStartGame() {
         System.out.println(START_MESSAGE);
+    }
+
+    public void printMovementMessage() {
+        System.out.println(MOVEMENT_MESSAGE);
     }
 }
