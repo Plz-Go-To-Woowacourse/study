6fb6781cf658d237016ea19d5a7a052f37ab21c3
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-03 13:26:58 +0900

    feat : 경주할 자동차 이름을 받아 RacingCar 객체 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.utils.Validator;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class InputView {
