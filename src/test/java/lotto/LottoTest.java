dd12468850dff8e84a8c3fc4834787c6ba8d3d5f
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-09 17:04:16 +0900

    test : 가진 로또 중 보너스 번호가 있는지 확인하는 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,8 @@ import lotto.domain.Lotto;
 import lotto.domain.vo.BonusNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
@@ -28,7 +30,7 @@ class LottoTest {
 
     // 아래에 추가 테스트 작성 가능
 
-    @DisplayName("당첨 번호와 보너스 번호를 받아 맞은 개수를 정확히 세는지")
+    @DisplayName("당첨 번호와 보너스 번호를 받고 보너스 번호를 제외하고 맞은 개수를 정확히 세는지")
     @Test
     void countNumberTest() {
         // given
@@ -42,5 +44,21 @@ class LottoTest {
 
         // then
         assertThat(actualCount).isEqualTo(3);
+    }
+
+    @DisplayName("가진 로또 중 보너스 번호가 있는지")
+    @ParameterizedTest
+    @CsvSource(value = {"6, true", "7, false"})
+    void isCorrectBonusNumberTest(String expectedBonus, Boolean expectedResult) {
+        // given
+        List<Integer> actualNumbers = List.of(1,2,3,4,5,6);
+        BonusNumber bonusNumber = new BonusNumber(expectedBonus);
+        Lotto lotto = new Lotto(actualNumbers);
+
+        // when
+        Boolean actual = lotto.isCorrectBonusNumber(bonusNumber);
+
+        // then
+        assertThat(expectedResult).isEqualTo(actual);
     }
 }
