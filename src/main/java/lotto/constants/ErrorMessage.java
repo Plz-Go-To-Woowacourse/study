91fd8bde1d7e7011e18c63cb39e0f1fc95bf6d56
ming-taro <hera002008@naver.com>
2023-12-09 14:08:12 +0900

    refactor: 상수클래스 enum형으로 변경

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,8 +1,8 @@
 package lotto.constants;
 
 public enum ErrorMessage {
-    INVALID_NUMBER("숫자를 입력해주세요."),
-    INVALID_LOTTO_PURCHASE_AMOUNT("로또는 1,000원 단위로 구매 가능합니다.");
+    INVALID_NUMBER("숫자를 입력해 주세요."),
+    INVALID_LOTTO_PURCHASE_AMOUNT("로또는 1,000원 단위로 구매 가능 합니다.");
 
     private final String PREFIX = "[ERROR]";
     private final String text;
--- src/main/java/lotto/constants/OutputMessage.java
+++ src/main/java/lotto/constants/OutputMessage.java
@@ -1,10 +1,21 @@
 package lotto.constants;
 
-public class OutputMessage {
-    public final static String PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
-    public final static String LOTTO_PURCHASE_COUNT = "%d개를 구매했습니다.";
-    public final static String WINNING_NUMBER_INPUT = "당첨 번호를 입력해 주세요.";
-    public final static String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
-    public final static String WINNING_STATISTIC = "당첨 통계";
-    public final static String RATE_OF_RETURN = "총 수익률은 62.5%입니다.";
+public enum OutputMessage {
+    PURCHASE_AMOUNT_INPUT("구입금액을 입력해 주세요."),
+    LOTTO_PURCHASE_COUNT("%d개를 구매했습니다."),
+    WINNING_NUMBER_INPUT("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요."),
+    WINNING_STATISTIC("당첨 통계"),
+    RATE_OF_RETURN("총 수익률은 62.5%입니다.")
+    ;
+
+    private final String text;
+
+    OutputMessage(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
