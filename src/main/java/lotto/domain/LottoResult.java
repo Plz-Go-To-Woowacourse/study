0d518cdba1508101db29277afdf53807a0dd61b9
bbbbooo <qlqlzbxkzl@naver.com>
2023-12-09 17:06:02 +0900

    refactor : 사용하지 않는 필드값 제거함

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,14 +1,9 @@
 package lotto.domain;
 
 public class LottoResult {
-    private final Integer correctNumber;
-    private final Boolean isCorrectBonusNumber;
     private final Reward reward;
 
-
     public LottoResult(Integer correctNumber, Boolean isCorrectBonusNumber) {
-        this.correctNumber = correctNumber;
-        this.isCorrectBonusNumber = isCorrectBonusNumber;
         this.reward = Reward.valueOf(correctNumber, isCorrectBonusNumber);
     }
 
